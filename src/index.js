7d30af693d5a53c6b829785cd7e3ef1026e1280c
Anirudh <anirudh.r@flipkart.com>
2017-08-18 16:22:34 +0530

    added bulma as css framework

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Index from '../src/components/index.js';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
+import './bulma.min.css';
+import './bulma.main.js';
 
 ReactDOM.render(
     <Provider>
