6a60c66f172684a56b6265aa5ab4f21de39f2dee
Anirudh <anirudh.r@flipkart.com>
2017-09-08 21:00:32 +0530

    created a store

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Index from '../src/components/index.js';
+import Index from './components/index';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from '../src/reducers/index'
 import thunk from 'redux-thunk'
-import reducer from '../src/reducers/index.js'
+
+// console.log(reducer)
 
 const logger = store => next => action => {
-    console.group(action.type)
-    console.info('dispatching', action)
-    let result = next(action)
-    console.log('next state', store.getState())
-    console.groupEnd(action.type)
-    return result
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
-    reducer,
-    composeEnhancers(
-        applyMiddleware(logger, thunk)
+  reducer,
+  composeEnhancers(
+    applyMiddleware(logger, thunk)
     )
-)
+  )
+
+// console.log(store.getState())
 
 ReactDOM.render(
+  <BrowserRouter>
     <Provider store={store}>
-        <BrowserRouter><Index /></BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
-);
-
+      <Index />
+    </Provider>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
+
